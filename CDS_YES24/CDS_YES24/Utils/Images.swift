295fb85a8da0679ea1e10c20ee7a9fb6186282fd
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-11-14 11:50:22 +0900

    [Add] #1 - 에셋 추가 - 컬러 에셋 추가 및 Constant 세팅 - 이미지 에셋 추가 및 Constant 세팅

---
+++ CDS_YES24/CDS_YES24/Utils/Images.swift
@@ -0,0 +1,30 @@
+//
+//  Image.swift
+//  CDS_YES24
+//
+//  Created by 강윤서 on 2022/11/14.
+//
+
+import UIKit
+
+public enum Image {
+    // TabBar
+    public static let category = UIImage(named: "category")
+    public static let home = UIImage(named: "home")
+    public static let myTicket = UIImage(named: "myTicket")
+    public static let search = UIImage(named: "search")
+    public static let selectedMyTicket = UIImage(named: "selectedMyTicket")
+    public static let selectedSearch = UIImage(named: "selectedSearch")
+    public static let seletedCategory = UIImage(named: "seletedCategory")
+    public static let seletedHome = UIImage(named: "seletedHome")
+    
+    // Home
+    
+    // Detail
+    public static let heart = UIImage(named: "heart")
+    public static let fillHeart = UIImage(named: "fillHeart")
+    
+    // Search
+    
+    // MyTicket
+}
